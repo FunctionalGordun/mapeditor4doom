2d6ab42ce280f5f9d46d107f393d6066ee409e47
Gordey <gordey.moskvichev@gmail.com>
2020-12-07 11:36:29 +0300

    valid  scene

--- src/main.c
+++ src/main.c
@@ -6,6 +6,52 @@ void i_error()
 	exit(1);
 }
 
+void check_scene(int max_x, int max_y, int min_x, int min_y, t_map *map)
+{
+	t_nod *n;
+	t_nod *tmp;
+	int i;
+	
+	n = map->nod;
+	tmp = map->nod;
+	i = 0;
+	while (n)
+	{
+		if (n->x1 == max_x || n->x2 == max_x || n->x1 == min_x || n->x2 == min_x ||
+		n->y1 == max_y || n->y2 == max_y || n->y1 == min_y || n->y2 == min_y)
+		{
+			tmp = n;
+			while (tmp)
+			{
+				if (tmp->x1 == max_x)
+					i++;
+				else if (tmp->x2 == max_x)
+					i++;
+				if (tmp->x1 == min_x)
+					i++;
+				else if (tmp->x2 == min_x)
+					i++;
+				if (tmp->y1 == max_y)
+					i++;
+				else if (tmp->y2 == max_y)
+					i++;
+				if (tmp->y1 == min_y)
+					i++;
+				else if (tmp->y2 == min_y)
+					i++;
+				tmp = tmp->nxt;
+			}
+			printf("i: %d\n", i);
+			if (i == 8 || i == 12 || i == 11 || i == 10) // ????????????maybe
+				printf("that's cool map!\n");
+			else
+				printf("close your fucking map!\n");
+			break ;
+		}
+		n = n->nxt;
+	}
+}
+
 int valid_map(t_map *map)
 {
 	t_nod *n;
@@ -39,8 +85,9 @@ int valid_map(t_map *map)
 			min_y = n->y2;
 		n = n->nxt;
 	}
-	printf("max x: %d y: %d\n", max_x, max_y);
-	printf("min x: %d y: %d\n", min_x, min_y);
+	check_scene(max_x, max_y, min_x, min_y, map);
+	// printf("max x: %d y: %d\n", max_x, max_y);
+	// printf("min x: %d y: %d\n", min_x, min_y);
 	return (0);
 }
 
@@ -96,7 +143,6 @@ int main(int ac, char **av)
 
 	if (events(map) == 1)
 	{
-		printf("save!\n");
 		valid_map(map);
 		writedown_map(map);
 	}
